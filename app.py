4398f842301b69ecb6254e7b675fc9b5fc737ca1
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-22 21:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -317,8 +317,4 @@ if __name__ == "__main__":
 
     demo.queue()
 
-    demo.launch(
-        server_name="0.0.0.0",
-        server_port=7860,
-        share=False
-    )
+    demo.launch(share=True)
