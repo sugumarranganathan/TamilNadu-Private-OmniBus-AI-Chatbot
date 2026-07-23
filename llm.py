71844499e178927af5818baa57dc62afa26a3b54
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-23 10:38:32 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,6 +1,8 @@
 import os
 import google.generativeai as genai
 
+from prompts import build_prompt
+
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
@@ -10,21 +12,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 def generate_answer(question, context):
 
-    prompt = f"""
-You are a Tamil Nadu Private OmniBus AI Assistant.
-
-Use ONLY the information below.
-
-Context:
-
-{context}
-
-Question:
-
-{question}
-
-If the answer is not available in the context, politely say so.
-"""
+    prompt = build_prompt(question, context)
 
     response = model.generate_content(prompt)
 
